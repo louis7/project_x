32de9094836f335c7c378972eff444163e65d883
prometheus <prometheus@prometheuss-MBP.home>
2020-02-13 02:12:05 -0500

    add config.public lince of code in config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.public_file_server.enabled = true
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -24,7 +23,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+# this code was specified by heroku app
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
